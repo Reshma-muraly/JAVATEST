dd2b62fac5bc0dd61d4a7f3a21ea529a5403b519
USER <USER@DESKTOP-ANL7QVP>
2024-06-10 18:43:52 +0530

    Test (1)

---
+++ MavenProject/src/main/java/inheritance/SingleInheritance.java
@@ -0,0 +1,25 @@
+package inheritance;
+
+public class SingleInheritance {
+
+	public void display(int a, int b)
+		{
+		int c= a+b;
+		System.out.println(c);
+		
+		}
+}
+
+  class Child
+ {
+	 
+ 
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingleInheritance obj= new SingleInheritance();
+				obj.display(2, 115);
+	}
+
+}
